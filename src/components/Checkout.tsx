cf2d9da543a46e4066d0c54cf2b88d0867a24dda
uniclique <unicliquetech@gmailcom>
2024-05-29 16:13:13 +0100

    use client configuration

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState, useEffect } from 'react';
 import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
